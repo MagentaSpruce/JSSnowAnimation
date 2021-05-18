e7bbc82bfcfb6c3cd49bb5a328e93839e897dece
MagentaSpruce <magentaspruce@protonmail.com>
2021-05-18 17:20:04 -0400

    Created and implemented the createSnowFlake() function. Finished.

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function createSnowFlake(){
+    const snowFlake = document.createElement('i');
+    snowFlake.classList.add('fas');
+    snowFlake.classList.add('fa-snowflake');
+    snowFlake.style.left = Math.random() * window.innerWidth + 'px';
+    snowFlake.style.animationDuration = Math.random() * 3 + 2 + 's';
+    snowFlake.style.opacity = Math.random();
+    snowFlake.style.fontSize = Math.random() * 10 + 10 + 'px'
+
+    document.body.appendChild(snowFlake);
+
+    setTimeout(() => {snowFlake.remove()}, 7000)
+}
+setInterval(createSnowFlake, 10);
